99f5a779e7d0522267d74508da581f89bfefa2bd
gabriellcolman <116891585+gabriellcolman@users.noreply.github.com>
2023-02-12 02:28:20 -0300

    Add files via upload

---
+++ ejercicios_de_openBoodcamp/ejercicio-4.js
@@ -0,0 +1,19 @@
+//cadena de texto 
+let str_nombre = "gabriel";
+let str_apellido = "colman";
+let str_concatenacion = "    estudiante";
+console.log(str_concatenacion.concat(" ",str_nombre," ",str_apellido))
+
+//mayus y minus
+let str_minus = "estudianteminus"
+let str_mayus = "estudianteMayus"
+console.log(str_minus.toLowerCase())
+console.log(str_mayus.toUpperCase())
+//numero de letras
+console.log(str_concatenacion.length)
+console.log(str_nombre.charAt(0))
+console.log(str_apellido[5])
+console.log(str_concatenacion.trimStart().length)
+
+
+
