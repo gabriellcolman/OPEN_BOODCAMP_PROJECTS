2778a83e36f45220f354240c3b6195a579ecdfa3
gabriellcolman <116891585+gabriellcolman@users.noreply.github.com>
2023-02-14 07:58:37 -0300

    Add files via upload

---
+++ ejercicios_de_openBoodcamp/ejercico-5(numeros_en_javascript).js
@@ -0,0 +1,12 @@
+let int_altura = 170;
+let int_edad = 28;
+let Float_altura = 1.70;
+let float_peso = 60.70;
+ 
+console.log (Float_altura.toFixed(1));
+console.log(float_peso.toPrecision(5))
+
+let max_valor_js = Number.MAX_VALUE;
+
+console.log(max_valor_js + 1);
+
